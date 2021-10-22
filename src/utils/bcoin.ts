2f8d21741ce0bbb9876f59420b1a3717532adf2e
Milan Steiner <steiner@travix-media.de>
2021-10-22 17:48:05 +0200

    Adding utils

--- src/utils/bcoin.ts
+++ src/utils/bcoin.ts
@@ -16,3 +16,23 @@ const clientOptions: ClientOptions = {
 
 // Open Client
 const client: NodeClient = new NodeClient(clientOptions)
+
+/**
+ * Get Total BlockCount
+ * 
+ * @returns Total Blockcount
+ */
+export const getBlockCount = async () : Promise<number> => {
+    return await client.execute('getblockcount')
+}
+
+/**
+ * Get the timestamp of a block
+ * 
+ * @param blockHeight
+ * @returns Timestamp of a block
+ */
+export const getBlockDate = async (blockHeight: number) : Promise<number> => {
+    const response = await client.execute('getblockbyheight', [blockHeight, true, false])
+    return response.time
+}
